d2c9fe19fa7487a6620599bb76ae9b4468b2d261
Himanshu kumar <hvk940@gmail.com>
2021-07-18 02:37:22 +0530

    push

--- header.js
+++ header.js
@@ -28,3 +28,48 @@ $(document).ready(function () {
     $(this).find("ul.dropdown-menu").removeClass("show open");
   });
 });
+
+
+$(document).ready(function () {
+  $("#owl-demo").owlCarousel({
+    navigation: true,
+    autoPlay:true
+  });
+});
+ let hstudents=0;
+ let interval=1;
+let student_interval= setInterval(() => {
+  hstudents+=20;
+    $("#happy-students").text(hstudents+"+");
+if (hstudents >= 10000) {
+  clearInterval(student_interval);
+}
+  }, interval);
+
+   let hours = 0;
+   let hours_interval = setInterval(() => {
+     hours += 200;
+     $("#hours").text(hours+"+");
+     if (hours >= 112000) {
+       clearInterval(hours_interval);
+     }
+   }, interval);
+
+
+   let cities = 0;
+   let cities_interval = setInterval(() => {
+     cities += 1;
+     $("#cities").text(cities+"+");
+     if (cities >= 500) {
+       clearInterval(cities_interval);
+     }
+   }, interval);
+
+   let rating = 0;
+   let rating_interval = setInterval(() => {
+     rating += 0.1;
+     $("#rating").text(rating.toFixed(1));
+     if (rating >= 4.8) {
+       clearInterval(rating_interval);
+     }
+   }, interval);
